23a258f59ecec31bd4ba2ecfe8daf0a923637d5c
OD <vhanxtreme@gmail.com>
2020-12-16 03:00:15 +0800

    OMA Resource Allocator will now balance between timeslots

--- od/network/allocator.py
+++ od/network/allocator.py
@@ -5,13 +5,12 @@ import od.vars as GV
 import sys
 
 
-
 # Resource Allocator for OMA
 class ResourceAllocatorOMA:
     # like a 2d rectangle:
     # height - the total bandwidth available
     # width - the total timeslots
-    def __init__(self, max_bandwidth,max_timeslots):
+    def __init__(self, max_bandwidth, max_timeslots):
         if(NET_TS_PER_NET_STEP != 2):
             GV.DEBUG.Log(
                 "Error!! This allocator only works on 2 timeslots per network step condition."
@@ -35,11 +34,14 @@ class ResourceAllocatorOMA:
                 self.alloc_resources[1] -= bandwidth
                 offset_timeslot = 0
         elif (bandwidth == NET_RB_BW_UNIT*2 and timeslots == 1):
+            max_res_ts = 0
             for ts in range(self.max_timeslots):
-                if(self.alloc_resources[ts] >= bandwidth):
-                    self.alloc_resources[ts] -= bandwidth
-                    offset_timeslot = ts
-                    break
+                if self.alloc_resources[ts] > self.alloc_resources[max_res_ts]:
+                    max_res_ts = ts
+
+            if(self.alloc_resources[max_res_ts] >= bandwidth):
+                self.alloc_resources[max_res_ts] -= bandwidth
+                offset_timeslot = max_res_ts
         else:
             GV.DEBUG.Log(
                 "Error!! This allocator only works on 2x1 or 1x2 resource block allocation"
